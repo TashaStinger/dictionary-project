da20d9f420793ff359f5e5e6ea57efbc4789fb76
TashaStinger <104356141+TashaStinger@users.noreply.github.com>
2022-08-10 12:44:37 +0200

    added the search engine

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Dictionary Project</h1>
-      </header>
+      <div className='container'>
+        <header className="App-header">
+          <h1>Dictionary Project</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className='App-footer'>
+          <small>Coded by Natalia Chaplia</small>
+        </footer>
+      </div>
     </div>
   );
 }
